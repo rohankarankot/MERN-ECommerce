7e4d17c1525985008d395d2c61238b3b934ae9a5
Rohan karankot <rohankarankot@gmail.com>
2022-05-21 16:59:35 +0530

    created add product route

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,8 +3,9 @@ const mongoose = require("mongoose");
 const connectDB = () => {
   mongoose
     .connect(process.env.MONGO_URI, {
-      usenewurlparser: true,
+      useNewUrlParser: true,
       useUnifiedTopology: true,
+      useCreateIndex: true,
     })
     .then((data) => {
       console.log("Connected to MongoDB" + data.connection.host);
--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,3 +1,14 @@
+const Product = require("../models/productModel");
+
+//create Product
+exports.createProduct = async (req, res, next) => {
+  const newProduct = new Product(req.body);
+  const savedProduct = await newProduct.save();
+  res.status(201).json({
+    success: true,
+    savedProduct,
+  });
+};
 exports.getAllProducts = (req, res) => {
   res.status(200).json({
     message: "All products fetched successfully",
