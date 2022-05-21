d720527bbe5a0209d6d8e3e734307ea398dd2288
Rohan karankot <rohankarankot@gmail.com>
2022-05-21 15:19:17 +0530

    Revert "created basic server"
    This reverts commit dcd9c042313d3dce39ec4f03518f0bc7b35b4313.

--- backend/server.js
+++
@@ -1,8 +0,0 @@
-const app = require("./app");
-const dotenv = require("dotenv");
-
-//configure dotenv
-dotenv.config({ path: "backend/config/config.env" });
-app.listen(process.env.PORT, () => {
-  console.log("Server is running on port " + process.env.PORT);
-});
